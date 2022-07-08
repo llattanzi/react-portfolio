8ce8ac514ef62aa6133653ec59a25b7f941c7cf8
llattanzi <lauren71442@gmail.com>
2022-07-07 20:32:59 -0400

    fix imports, render footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from './components/Header';
 import About from './components/About';
 import Project from './components/Project';
 import ContactForm from './components/Contact';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
 
 function App() {
   const [currentPage, handlePageChange] = useState('About');
@@ -29,6 +31,9 @@ function App() {
       <main class="main-content">
         <div>
           {renderPage()}
+        </div>
+        <div>
+          <Footer/>
         </div>
       </main>
     </div>
