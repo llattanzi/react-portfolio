2862ddabce00e568a862e25331edd942800bd19f
llattanzi <lauren71442@gmail.com>
2022-07-28 15:41:48 -0400

    add icons to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const Footer = () => {
   return (
     <footer>
         <div className="footer-container">
-            <div>
-                <a href="https://github.com/llattanzi">GitHub</a>
-            </div>
-            <div>
-                <a href="https://www.linkedin.com/in/lauren-lattanzi/">LinkedIn</a>
-            </div>
-            <div>
-                <a href="https://stackoverflow.com/users/17769864/lauren-lattanzi">Stack Overflow</a>
-            </div>
+            <a href="https://github.com/llattanzi">
+                <GitHubIcon style={{ color: '#ffffff'}} className="icons"/>
+                <p>
+                    GitHub
+                </p>
+            </a>
+
+            <a href="https://www.linkedin.com/in/lauren-lattanzi/">
+                <LinkedInIcon style={{ color: '#ffffff' }} className="icons" />
+                <p>
+                    LinkedIn
+                </p>
+            </a>
+
+            <a href="https://stackoverflow.com/users/17769864/lauren-lattanzi">
+                <p>
+                    Stack Overflow
+                </p>
+            </a>
         </div>
     </footer>
   );
