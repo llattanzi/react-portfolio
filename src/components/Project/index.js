a6c0d8b48c16cb46bd5d6772235515d434b2b356
llattanzi <lauren71442@gmail.com>
2022-07-28 19:42:02 -0400

    create project styling

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import project1 from "../../assets/images/project-1.jpg";
 import project2 from "../../assets/images/project-2.jpg";
 import project3 from "../../assets/images/project-3.jpg";
@@ -19,12 +21,27 @@ function Project() {
                     className='project-photo' 
                 />
                 <CardContent className="content-info">
-                <a href="https://pacific-depths-60618.herokuapp.com/">
-                    <div>
-                        <h3>Roam</h3>
-                        <h4>MongoDb, Express.js, React, Node.js, Apollo-GraphQL, MUI</h4>
-                    </div>
-                </a>
+                    <h2>Roam</h2>
+                    <p>
+                        Roam is an app for people who want to travel sustainably. Traveling is great and we love it — but it also takes a toll on the planet. We wanted to create a fun, user-friendly application so people can travel while being mindful of climate change.
+                    </p>
+                    <p>
+                        Users can create an account, create trip itineraries with friends, track expenses and split them at the end of their trip. Users can even offset the carbon emissions from their trip by donating to a charity, all through Roam.
+                    </p>
+                    <h4>Technologies Used:</h4>
+                    <p> MongoDb, Express.js, React, Node.js, Apollo-GraphQL, MUI</p>
+                <div className='button-container'>
+                    <Button 
+                        variant='contained' 
+                        href='https://pacific-depths-60618.herokuapp.com/'
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="button"
+                    >
+                        View Application
+                        <OpenInNewIcon style={{ color: '#ffffff' }} className='icon-open'/>
+                    </Button>
+                </div>
                 </CardContent>
             </Card>
 
@@ -32,7 +49,7 @@ function Project() {
                 <CardContent className="content-info">
                 <a href="https://triangle-twitchers.herokuapp.com/">
                     <div>
-                        <h3>Twitcher</h3>
+                        <h2>Twitcher</h2>
                         <h4>CSS, JavaScript, Node.js, Handlebars.js, MySQL</h4>
                     </div>
                 </a>
@@ -49,7 +66,7 @@ function Project() {
                 <CardContent className="content-info">
                 <a href="https://llattanzi.github.io/run-buddy/">
                     <div >
-                        <h3>Run Buddy</h3>
+                        <h2>Run Buddy</h2>
                         <h4>HTML/CSS</h4>
                     </div>    
                 </a>
@@ -60,7 +77,7 @@ function Project() {
                 <CardContent className="content-info">
                 <a href="https://llattanzi.github.io/robot-gladiators/">
                     <div>
-                        <h3>Robot Gladiators</h3>
+                        <h2>Robot Gladiators</h2>
                         <h4>JavaScript</h4>
                     </div>
                 </a>
@@ -77,7 +94,7 @@ function Project() {
                 <CardContent className="content-info">
                 <a href="https://foglea10.github.io/Note-Eater/">
                     <div>
-                        <h3>Note Eater</h3>
+                        <h2>Note Eater</h2>
                         <h4>HTML, CSS, JavaScript</h4>
                     </div>
                 </a>
