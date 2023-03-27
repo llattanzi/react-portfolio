11314ecf6c96a7731715163e7643bfdac1e81f71
Lauren Lattanzi <lauren71442@gmail.com>
2023-03-27 14:54:12 -0400

    update about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -16,10 +16,10 @@ function About() {
       <CardContent className="content-info">
         <h2>Who am I?</h2>
         <p>
-          I am a Product Development Engineer and Full Stack Web Developer with a passion for knowledge and creativity. 
+          I am a Software Engineer with expertise in Full Stack Web Development and a passion for knowledge and creativity. 
           I love collaborating on teams and tackling any kind of challenge whether it's learning something completely new or taking a deeper dive into my existing skills.
-          I hope to bring a new creative take and a fresh, innovative perspective into a career as a full stack developer with my experience designing a wide variety of medical devices.
-          As an engineer with an education in Biomedical Engineering and Full Stack Web Development,  
+          I hope to bring a new creative take and a fresh, innovative perspective to my career in software development with my background in product development designing medical devices and experience in Full Stack Web Development.
+          As an engineer with an education in Biomedical Engineering and Web Development,  
           I welcome and enjoy the challenge of solving complex problems.
           <br /><br />
           When I'm not coding, I spend my free time weightlifting, creating fitness programs, hiking, skiing, and traveling the world as much as I can.
